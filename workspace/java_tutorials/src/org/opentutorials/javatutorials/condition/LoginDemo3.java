eac7db902a7e89554783b9813745321b5e417ed7
jihyeon <wisedew2@gmail.com>
2023-10-20 15:40:55 +0900

    10.20

---
+++ workspace/java_tutorials/src/org/opentutorials/javatutorials/condition/LoginDemo3.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.condition;
+
+public class LoginDemo3 {
+
+	public static void main(String[] args) {
+		String id = args[0];
+        String password = args[1];
+        if (id.equals("egoing") && password.equals("111111")) { //논리연산자 and
+            System.out.println("right");
+        } else {
+            System.out.println("wrong");
+        }
+	}
+
+}
